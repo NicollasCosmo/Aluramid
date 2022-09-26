0f8c05cb06ed4fb36669155458a1a190e0a3d9d4
NicollasCosmo <111130572+NicollasCosmo@users.noreply.github.com>
2022-09-26 11:21:15 -0300

    atualização - JS

--- arquivos/main.js
+++ arquivos/main.js
@@ -1,6 +1,14 @@
-function tocaSom (idElementoAudio) {
+function tocaSom (seletordeAudio) {
 
-    document.querySelector(idElementoAudio).play();
+    const elemento =  document.querySelector(seletordeAudio);
+
+
+    if (elemento != null && elemento.localName === 'audio') {
+         elemento.play();
+    }
+    else {
+        console.log('Elemento não encontrado ou seletor inválido'); 
+    }
 
 }
 
@@ -13,12 +21,30 @@ for (let contador = 0; contador < ListaDeTeclas.length; contador++) {
     const instrumento = tecla.classList[1];
     const idAudio = `#som_${instrumento}`;  //template string
 
-
     tecla.onclick = function () {
 
         tocaSom(idAudio);   
     }
+
+
+    tecla.onkeydown = function (evento) {
+
+        if (evento.code == 'Space' || evento.code == 'Enter') {
+            tecla.classList.add('ativa');
+        }
+       
+    }
+
+    tecla.onkeyup = function () {
+        tecla.classList.remove('ativa');
+    }
 }
+
+
+
+
+ 
+
 
 
 
